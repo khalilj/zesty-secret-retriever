4cf9795b98a9b0507b98e40f353d86d5e6722a5d
Khalil Jiries <khalil.jiries@philips.com>
2022-11-30 13:27:04 +0200

    Implement /secret endpoint

--- TODO.md
+++ TODO.md
@@ -3,8 +3,8 @@
 * Dockerize the app - DONE
 * Create a docker registry - DONE
 * Create a CI and push to docker registry - DONE
-* Implement docker compose with DynamoDB instance zestyco/namodb-challenge:amd
-* Implement connectivity from Flask app to mondo DB
-* Implement /secret endpoint
+* Implement docker compose with DynamoDB instance zestyco/namodb-challenge:amd - DONE
+* Implement connectivity from Flask app to mondo DB - DONE
+* Implement /secret endpoint - DONE
 * Implement test part of CI
 * Don't push to registry in case the test fails
--- app/routes.py
+++ app/routes.py
@@ -1,16 +1,27 @@
 from app import app
 import os
+import boto3
 
 @app.route('/health')
 def health():
     return {
         "status": "Healthy!",
-        "container": os.environ.get('CONTAINER_IMAGE', '"khalilj/zesty-secret-retriever"')
+        "container": os.environ.get('CONTAINER_IMAGE', 'khalilj/zesty-secret-retriever')
     }
 
 @app.route('/secret')
 def secret():
+    code_name = os.environ.get('CODE_NAME')
+    dynamodb = boto3.client('dynamodb', region_name='us-west-2',
+        endpoint_url=os.environ.get('DYNAMO_ENDPOINT', 'http://localhost:8000'))
+    response = dynamodb.get_item(
+        TableName='devops-challenge',
+        Key={
+            'codeName': {'S': code_name}
+        }
+    )
+
     return {
-        "codeName": "<YOUR_CODENAME>",
-        "secretCode": "<SECRET_CODE>"
+        "codeName": code_name,
+        "secretCode": response['Item']['secretCode']['S']
     }

9c8db6a9ca8b6e01130002bce60756fa00c7a17e
Khalil Jiries <khalil.jiries@philips.com>
2022-11-30 13:50:08 +0200

    fix ./verification.sh

--- verification.sh
+++ verification.sh
@@ -5,7 +5,7 @@ docker-compose up --build -d
 sleep 5
 
 # Query results
-curl 127.0.0.1:5000/secret | jq '.secret_code'
+curl 127.0.0.1:5000/secret | jq '.secretCode'
 curl 127.0.0.1:5000/health | jq '.'
 
 # Destroy 
